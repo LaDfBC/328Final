c632c55849127e37d6814f0a2563fb2b965a6f73
George Mausshardt <george.mausshardt@mst.edu>
2013-05-02 14:34:02 -0500

    Correct Solver Shell

---
+++ solver.h
@@ -0,0 +1,29 @@
+/*
+* Programmer: George Mausshardt and Matt Lindsay
+* Date: 05/12/2013
+* FileName: solver.h
+* Purpose: Header file for the base class representing any number of
+* differential equation approximaters
+*/
+
+#ifndef SOLVER_H
+#define SOLVER_H
+
+#include "operator_library.h"
+
+template <class U, class T, class V>
+class Solver
+{
+  public:
+    ostream& output_stream(ostream& out) const = 0;
+    int get_convergence_speed() = 0;
+    U& operator()() = 0;
+
+  private:
+    V m_a_matrix;
+    U m_b_vector;
+    U m_x_vector;
+    int m_convergence_speed;
+};
+
+#endif //SOLVER_H
